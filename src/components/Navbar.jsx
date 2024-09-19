af6587921d10de9a38218cd3c64ecb1864a7cd54
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2024-09-19 09:37:39 +0530

    Navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar(){
+    return(
+        <div className="navbar">
+            <img src="..\src\assets\troll-face.png" className="logo"/>
+            <h1 className="title">Meme Generator</h1>
+            <p className="sub-title">React Portfolio Project</p>
+        </div>
+    )
+}
+
+export default Navbar
