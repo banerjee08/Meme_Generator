8dd54006f216e6452618ece177f07f15d6e29023
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2024-11-17 04:23:23 +0530

    html and css done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,22 @@
-// import { useState } from 'react'
-// import './App.css'
+function Main(){
+    return(
+        <div className="container">
+            <div className="input-container">
+                <p className="top-text--heading">Top text</p>
+                <p className="bottom-text--heading">Bottom text</p>
+                <input className="top-text--input"></input>
+                <input className="bottom-text--input"></input>
+                <button className="btn">Get a new meme image</button>
+            </div>
 
-// function App() {
+            <div className="meme-container">
+                <img src="https://assets.entrepreneur.com/content/3x2/2000/20180703190744-rollsafe-meme.jpeg?format=pjeg&auto=webp&crop=4:3" className="meme--img"/>
+                <p className="meme--top-text">Top Text</p>
+                <p className="meme--bottom-text">And take my money</p>
+            </div>
 
-//   return (
-//     <>
-//       <h1>Meme Generator</h1>
-//     </>
-//   )
-// }
+        </div>
+    )
+}
 
-// export default App
+export default Main
